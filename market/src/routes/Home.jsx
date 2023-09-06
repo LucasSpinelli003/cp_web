17c3517212a088e24f5b5872ff34d4a4b4148dde
LucasSpinelli003 <rm97757@fiap.com.br>
2023-09-06 02:27:54 -0300

    Home adicionado

--- market/src/routes/Home.jsx
+++ market/src/routes/Home.jsx
@@ -1,6 +1,15 @@
+import banner from'../assets/img/banner_img.jpg'
+import {Link} from 'react-router-dom';
+
 export default function Home() {
     return(
        <>
+    <body>
+        <section>
+            <Link to='/aparelho'><img className='banner' src={banner} alt="Banner"/></Link>
+        </section>
+    </body>
+    
        </>
     )
 }
