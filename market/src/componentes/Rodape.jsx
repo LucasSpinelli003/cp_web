f4ef31d52025d51b64c438d71e71863f55dbc71a
LucasSpinelli003 <rm97757@fiap.com.br>
2023-09-06 02:28:06 -0300

    Rodapé adicionado

---
+++ market/src/componentes/Rodape.jsx
@@ -0,0 +1,12 @@
+import './Rodape.css'
+
+export default function Rodape(){
+    return(
+    <>
+    <footer>
+        <p>KaBuM!® é uma marca registrada de KABUM S.A | CNPJ: 05.570.714/0001-59 | Todos os direitos reservados. Os preços anunciados neste site ou via e-mail promocional podem ser alterados sem prévio aviso. O KaBuM! não é responsável por erros descritivos. As fotos contidas nesta página são meramente ilustrativas do produto e podem variar de acordo com o fornecedor/lote do fabricante. Este site trabalha 100% em criptografia SSL.  Clique aqui e veja as políticas de nossa empresa.
+        </p>
+    </footer>
+    </>
+    )
+}
